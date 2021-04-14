f1b8011104736a2059a04b0938837ea601eacff7
wong lik <wonglik0817@outlook.com>
2021-04-14 11:16:58 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        Dim y As Integer
+
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+        y = 0
+
+        Array.Reverse(strNamaHari)
+        For y = 0 To 6
+            lstResult.Items.Add(strNamaHari(y))
+        Next
+    End Sub
+End Class
